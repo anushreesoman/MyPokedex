5b2c98da1677c3065cfe7f51334c56aec94f83ea
Anushree Soman <ANUSHREE.SOMAN@BARUCHMAIL.CUNY.EDU>
2021-08-06 16:41:53 -0400

    Initial Commit

---
+++ MyPokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPokedex
+//
+//  Created by Anushree Soman on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPokedex/MyPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokedexApp.swift
+//  MyPokedex
+//
+//  Created by Anushree Soman on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MyPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
